3b4bce6c23f72b8e16ced2be9876288c44f5757b
kira134679 <kira134679@gmail.com>
2026-01-20 00:52:10 +0800

    fix api

--- src/api/products.js
+++ src/api/products.js
@@ -5,7 +5,7 @@ const ADMIN_API_PATH = `/api/${import.meta.env.VITE_API_PATH}/admin`;
 export const productsApi = {
   getAllProducts: () => apiClient.get(`${ADMIN_API_PATH}/products/all`),
   getProducts: (params = {}) => apiClient.get(`${ADMIN_API_PATH}/products`, { params }),
-  addProduct: data => apiClient.post(`${ADMIN_API_PATH}/products`, { data }),
-  editProduct: (id, data) => apiClient.put(`${ADMIN_API_PATH}/product/${id}`, { data }),
+  createProduct: data => apiClient.post(`${ADMIN_API_PATH}/product`, { data }),
+  updateProduct: (id, data) => apiClient.put(`${ADMIN_API_PATH}/product/${id}`, { data }),
   deleteProduct: id => apiClient.put(`${ADMIN_API_PATH}/product/${id}`),
 };
