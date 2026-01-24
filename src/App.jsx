ded8d129c7fea637135f4a20d2a43abb52d281d6
kira134679 <kira134679@gmail.com>
2026-01-24 12:06:30 +0800

    feat: Login 元件化

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Modal } from 'bootstrap';
 import { useEffect, useRef, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { authApi, productsApi } from './api';
+import Login from './components/Login';
 
 const TEMP_PRODUCT_DATA = {
   title: '',
@@ -17,11 +18,6 @@ const TEMP_PRODUCT_DATA = {
 };
 
 function App() {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-  });
-
   const [isAuth, setIsAuth] = useState(false);
   const [isAuthChecked, setIsAuthChecked] = useState(false);
 
@@ -34,28 +30,6 @@ function App() {
   const [mode, setMode] = useState('');
 
   // form start
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    try {
-      const res = await authApi.login(formData);
-
-      const { token, expired } = res;
-      document.cookie = `hex_token=${token}; expires=${new Date(expired)}`;
-      setIsAuth(true);
-    } catch (error) {
-      setIsAuth(false);
-      toast.error(error);
-    } finally {
-      setIsAuthChecked(true);
-    }
-  };
-
-  const handleInputChange = e => {
-    const { name, value } = e.target;
-    setFormData(prevData => ({ ...prevData, [name]: value }));
-  };
 
   const handleModalInputChange = e => {
     const { name, value, type, checked } = e.target;
@@ -244,46 +218,7 @@ function App() {
           </div>
         </div>
       ) : (
-        <div className="container login">
-          <div className="row justify-content-center">
-            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div className="col-8">
-              <form id="form" className="form-signin" onSubmit={handleSubmit}>
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="username"
-                    name="username"
-                    placeholder="name@example.com"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                    required
-                    autoFocus
-                  />
-                  <label htmlFor="username">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <button className="btn btn-lg btn-primary w-100 mt-3" type="submit">
-                  登入
-                </button>
-              </form>
-            </div>
-          </div>
-          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
-        </div>
+        <Login setIsAuth={setIsAuth} setIsAuthChecked={setIsAuthChecked} />
       )}
       <div
         id="productModal"
