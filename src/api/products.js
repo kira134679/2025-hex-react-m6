596ad3dd9c4ff6ddd320801392ecd3000ae6b963
kira134679 <kira134679@gmail.com>
2026-02-07 02:27:20 +0800

    feat(api): 重命名以區分不同權限 API

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,7 @@
 import { ADMIN_API_PATH } from './constants';
 import { adminClient } from './instances/adminClient';
 
-export const productsApi = {
+export const adminProductsApi = {
   getAllProducts: () => adminClient.get(`${ADMIN_API_PATH}/products/all`),
   getProducts: (params = {}) => adminClient.get(`${ADMIN_API_PATH}/products`, { params }),
   createProduct: data => adminClient.post(`${ADMIN_API_PATH}/product`, { data }),
