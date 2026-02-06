598fa89f0c353355e073eeeb78ec7963dcf65588
kira134679 <kira134679@gmail.com>
2026-02-07 02:32:19 +0800

    feat(api): 新增 guestProductsApi

--- src/api/products.js
+++ src/api/products.js
@@ -1,5 +1,6 @@
-import { ADMIN_API_PATH } from './constants';
+import { ADMIN_API_PATH, GUEST_API_PATH } from './constants';
 import { adminClient } from './instances/adminClient';
+import { guestClient } from './instances/guestClient';
 
 export const adminProductsApi = {
   getAllProducts: () => adminClient.get(`${ADMIN_API_PATH}/products/all`),
@@ -7,4 +8,12 @@ export const adminProductsApi = {
   createProduct: data => adminClient.post(`${ADMIN_API_PATH}/product`, { data }),
   updateProduct: (id, data) => adminClient.put(`${ADMIN_API_PATH}/product/${id}`, { data }),
   deleteProduct: id => adminClient.delete(`${ADMIN_API_PATH}/product/${id}`),
+};
+
+export const guestProductsApi = {
+  getAllProducts: (config = {}) => guestClient.get(`${GUEST_API_PATH}/products/all`, config),
+  getProducts: (params = {}) => {
+    return guestClient.get(`${GUEST_API_PATH}/products`, { params });
+  },
+  getProductById: (productId, config = {}) => guestClient.get(`${GUEST_API_PATH}/product/${productId}`, config),
 };
