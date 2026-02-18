db4bc4f98b1a266c2afe2354543277746a5c9293
kira134679 <kira134679@gmail.com>
2026-02-18 14:43:24 +0800

    feat: 新增商品詳細

--- src/pages/Frontend/ProductDetail.jsx
+++ src/pages/Frontend/ProductDetail.jsx
@@ -1,3 +1,17 @@
+import { useLocation, useNavigate, useNavigation } from 'react-router';
+import NotFound from './NotFound';
+
 export default function ProductDetail() {
-  return <h2>商品詳細</h2>;
+  const { state } = useLocation();
+  const productData = state.product ?? null;
+
+  if (!productData) {
+    return <NotFound />;
+  } else {
+    return (
+      <div className="container">
+        <h2>{productData.title}</h2>
+      </div>
+    );
+  }
 }
