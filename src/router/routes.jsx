8543ae468cf99e162ccff7785dc91cf01525e980
kira134679 <kira134679@gmail.com>
2026-02-20 16:11:13 +0800

    feat(routes): 設定 login, admin 路由

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,5 +1,7 @@
 import App from '../App';
+import AdminLayout from '../layouts/AdminLayout';
 import FrontendLayout from '../layouts/FrontendLayout';
+import Login from '../pages/Admin/Login';
 import Cart from '../pages/Frontend/Cart';
 import Home from '../pages/Frontend/Home';
 import NotFound from '../pages/Frontend/NotFound';
@@ -18,7 +20,13 @@ export const routes = [
           { path: 'products', Component: Products },
           { path: 'products/:productId', Component: ProductDetail },
           { path: 'cart', Component: Cart },
+          { path: 'login', Component: Login },
         ],
+      },
+      {
+        path: 'admin',
+        Component: AdminLayout,
+        children: [{ path: 'products', Component: () => <h2>admin product</h2> }],
       },
     ],
   },
