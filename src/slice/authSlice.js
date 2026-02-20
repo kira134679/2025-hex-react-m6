33a98bfebf64974f2a2dfff9e486484af655f52b
kira134679 <kira134679@gmail.com>
2026-02-20 15:40:09 +0800

    feat: 新增 authSlice

---
+++ src/slice/authSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    isAuth: false,
+  },
+  reducers: {},
+});
+
+export default authSlice.reducer;
