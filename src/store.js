d39f0f28d176b2e31ba35b53991dd37742e93c0c
kira134679 <kira134679@gmail.com>
2026-02-21 23:40:46 +0800

    feat: store 加入 slice

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import authReducer from '@/slice/authSlice';
+import cartReducer from '@/slice/cartSlice';
+import productReducer from '@/slice/productSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    cart: cartReducer,
+    product: productReducer,
   },
 });
 
