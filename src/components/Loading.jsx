49a2aa66e829a5ec013e4067889c3390adbad9ac
kira134679 <kira134679@gmail.com>
2026-02-21 23:39:20 +0800

    fix: 修正 Loading 元件 z-index

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -8,7 +8,7 @@ export default function Loading({ isLoading = false }) {
       width="80"
       color="#4fa94d"
       ariaLabel="oval-loading"
-      wrapperStyle={{ zIndex: 50, inset: 0 }}
+      wrapperStyle={{ zIndex: 2000, inset: 0 }}
       wrapperClass="position-fixed bg-white bg-opacity-50 justify-content-center align-items-center"
     />
   );
