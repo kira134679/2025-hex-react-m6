213cf1f7444cb1e5b05ad49f5e655cc8c6abca47
kira134679 <kira134679@gmail.com>
2026-02-21 21:34:32 +0800

    feat: 新增 Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router';
 import './assets/styles/index.scss';
 import { router } from './router';
-import { Provider } from 'react-redux';
 import store from './store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <Toaster />
       <RouterProvider router={router}></RouterProvider>
     </Provider>
   </StrictMode>,
