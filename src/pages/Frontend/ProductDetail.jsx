f810c3637ff4cb3cfee0726240ab73f9d7bbea4c
kira134679 <kira134679@gmail.com>
2026-02-21 23:41:24 +0800

    feat: 調整 ProductDetail

--- src/pages/Frontend/ProductDetail.jsx
+++ src/pages/Frontend/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate, useNavigation } from 'react-router';
+import { useLocation } from 'react-router';
 import NotFound from './NotFound';
 
 export default function ProductDetail() {
@@ -9,7 +9,7 @@ export default function ProductDetail() {
     return <NotFound />;
   } else {
     return (
-      <div className="container">
+      <div className="container py-5">
         <h2>{productData.title}</h2>
       </div>
     );
