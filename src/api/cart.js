3bbe40e868bc936e769da0faf79b0e2e9f713135
kira134679 <kira134679@gmail.com>
2026-02-21 22:13:34 +0800

    fix(api): 修正 cartApi

--- src/api/cart.js
+++ src/api/cart.js
@@ -4,7 +4,7 @@ import { guestClient } from './instances/guestClient';
 export const guestCartApi = {
   addToCart: data => guestClient.post(`${GUEST_API_PATH}/cart`, data),
   getCart: () => guestClient.get(`${GUEST_API_PATH}/cart`),
-  updateCart: id => guestClient.put(`${GUEST_API_PATH}/cart/${id}`, data),
+  updateCart: (id, data) => guestClient.put(`${GUEST_API_PATH}/cart/${id}`, data),
   deleteCartItem: id => guestClient.delete(`${GUEST_API_PATH}/cart/${id}`),
   deleteCart: () => guestClient.delete(`${GUEST_API_PATH}/carts`),
 };
