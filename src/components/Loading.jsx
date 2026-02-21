e34389fc6a2d5d0ca9b8cc5df5456d2faec72ab4
kira134679 <kira134679@gmail.com>
2026-02-21 21:34:10 +0800

    feat: 新增 Loading 元件

---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import { Oval } from 'react-loader-spinner';
+
+export default function Loading({ isLoading = false }) {
+  return (
+    <Oval
+      visible={isLoading}
+      height="80"
+      width="80"
+      color="#4fa94d"
+      ariaLabel="oval-loading"
+      wrapperStyle={{ zIndex: 50, inset: 0 }}
+      wrapperClass="position-fixed bg-white bg-opacity-50 justify-content-center align-items-center"
+    />
+  );
+}
